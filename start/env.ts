67edbc8bdf7a980cc95bc896184764bb5a0b1a3c
Chris Sherwood <chris@crosstalksolutions.com>
2026-01-21 12:15:49 -0800

    fix: Better env variable error messages

--- start/env.ts
+++ start/env.ts
@@ -2,8 +2,8 @@ import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
-  APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.string(),
+  PORT: Env.schema.number({ message: 'PORT must be defined' }),
+  APP_KEY: Env.schema.string({ message: 'APP_KEY must be defined' }),
+  HOST: Env.schema.string({ format: 'host', message: 'HOST must be defined' }),
+  LOG_LEVEL: Env.schema.string.optional(),
 })
